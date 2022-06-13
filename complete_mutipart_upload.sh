519e5d6ebbb367de4992458f5a1b8e10f942ffc9
Adam Siwiec <73085927+adamsiwiec1@users.noreply.github.com>
2022-06-13 15:07:25 -0400

    Delete complete_mutipart_upload.sh

--- complete_mutipart_upload.sh
+++
@@ -1,6 +0,0 @@
-upload_file="$1"
-bucketName="$2"
-file="$3"
-upload_id="$4"
-
-aws s3api complete-multipart-upload --multipart-upload file://$upload_file --bucket $bucketName --key $file --upload-id $upload_id
